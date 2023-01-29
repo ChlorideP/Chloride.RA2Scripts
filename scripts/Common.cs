f994d3ea8726421930431855e2173bd175211de2
Chloride1 <caclx@outlook.com>
2023-01-30 01:32:14 +0800

    查询子ini读取顺序（可能有误差）

--- scripts/Common.cs
+++ scripts/Common.cs
@@ -27,4 +27,42 @@ public static class Common
         try { return sect[key].ToString(); }
         catch { return null; }
     }
+
+    /// <summary>
+    /// 从某一个ini文件开始，先序遍历子 ini 树。
+    /// </summary>
+    /// <param name="root">起始 ini 文件路径。</param>
+    /// <returns></returns>
+    public static List<FileInfo> TryGetIncludes(string root)
+    {
+        T Pop<T>(List<T> lst, int index = -1) {
+            var ret = lst.Last();
+            lst.RemoveAt(index >= 0 ? index : lst.Count + index);
+            return ret;
+        }
+
+        List<FileInfo> ret = new();
+        List<string> stack = new() { root };
+
+        var rootdir = Path.GetDirectoryName(root);
+
+        while (stack.Count > 0) {
+            FileInfo ini = new(Pop(stack));
+            IniDoc doc = new();
+
+            try {
+                doc.Deserialize(ini);
+            } catch (Exception e) {
+                Console.WriteLine($"!) Unable to open {ini.Name}: {e.ToString().Split(':').First()}");
+                continue;
+            }
+
+            ret.Add(ini);
+
+            if (doc.Contains("#include", out IniSection? inc))
+                stack.AddRange(inc!.Values.Select(i => Path.Combine(rootdir!, i)).Reverse());
+        }
+
+        return ret;
+    }
 }

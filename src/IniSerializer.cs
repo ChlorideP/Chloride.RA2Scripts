7fa787c76af05421d296a2dde9da6abdca4f3a9d
Chloride1 <caclx@outlook.com>
2023-02-27 20:42:43 +0800

    分离文本处理业务

--- src/IniSerializer.cs
+++ src/IniSerializer.cs
@@ -19,58 +19,68 @@ public static class IniSerializer
 
     public static void Deserialize(IniDoc doc, TextReader tr)
     {
-        IniSection? self = doc.Default, super = null;
+        IniSection? self = doc.Default;
 
         while (tr.Peek() > 0)
         {
+            // there must be faster ways to read ini chars while its structure still be kept...
+            // though I didn't figure out yet.
+
             var line = tr.ReadLine();
             var strip = line?.Trim();
 
             if (string.IsNullOrEmpty(strip) || strip.StartsWith(';'))
-            {
                 self!.Add(line);
-                continue;
-            }
-
-            if (strip.StartsWith('['))
-            {
-                var sect = strip.Split(';', 2);
-                var curSect = sect.First().Split(':').Select(i => i.Trim()[1..^1]).ToArray();
-                var curDesc = sect.ElementAtOrDefault(1);
-
-                if (doc.Contains(curSect[0], out self))
-                    continue;
-
-                if (curSect.Length > 1)
-                {
-                    _ = doc.Contains(curSect[1], out super);
-                    super ??= new(curSect[1]);
-                }
-                else super = null;
-
-                self = new(curSect[0], curDesc, super);
-                doc.Add(self);
-            }
+            else if (strip.StartsWith('['))
+                self = doc.ParseSection(strip);
             else if (strip.Contains('='))
-            {
-                var pair = strip.Split('=', 2);
-                pair[0] = pair[0].Trim();
-
-                if (pair[0].Contains(';'))
-                    continue;
-
-                var key = pair[0] == "+" ? $"+{doc.Diff++}" : pair[0];
-                var val = pair[1].Split(';', 2)[0];
-                var value = val.Trim();
-                var desc = string.IsNullOrEmpty(value) ? pair[1] : new StringBuilder(pair[1])
-                    .Replace(value, string.Empty, 0, val.Length)
-                    .ToString();
-
-                self!.Add(key, value, desc);
-            }
+                doc.ParseEntry(self, strip);
         }
 
         tr.Close();
+    }
+
+    private static IniSection ParseSection(this IniDoc doc, string sectionLine)
+    {
+        IniSection? super = null;
+
+        var sect = sectionLine.Split(';', 2);
+        var curSect = sect.First().Split(':').Select(i => i.Trim()[1..^1]).ToArray();
+        var curDesc = sect.ElementAtOrDefault(1);
+
+        if (!doc.Contains(curSect[0], out IniSection? self))
+        {
+            if (curSect.Length > 1)
+            {
+                _ = doc.Contains(curSect[1], out super);
+                super ??= new(curSect[1]);
+            }
+
+            self = new(curSect[0], curDesc, super);
+            doc.Add(self);
+        }
+
+        return self!;
+    }
+
+    private static void ParseEntry(this IniDoc doc, IniSection self, string entryLine)
+    {
+        var pair = entryLine.Split('=', 2);
+        pair[0] = pair[0].Trim();
+
+        if (pair[0].Contains(';'))
+            return;
+
+        var val = new StringBuilder(pair[1]);
+        var value = pair[1].Split(';', 2)[0];
+
+        self.Add(
+            pair[0] == "+" ? $"+{doc.Diff++}" : pair[0],
+            value.Trim(),
+            string.IsNullOrEmpty(value)
+                ? val.ToString()
+                : val.Replace(value.Trim(), string.Empty, 0, value.Length).ToString()
+        );
     }
 
     public static void Serialize(this IniDoc doc, FileInfo iniFile, string encoding = "utf-8", string pairing = "=")
